a8d14a33ed140f990fa30ad4d6fb8e7535864bed
nihaonihao <123456789@qq.com>
2018-05-14 18:52:22 +0800

    XINZENG

--- demo-provider/src/main/java/com/maxnerva/aapcaliction/ProUserApp.java
+++ demo-provider/src/main/java/com/maxnerva/aapcaliction/ProUserApp.java
@@ -1,8 +1,8 @@
 package com.maxnerva.aapcaliction;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
